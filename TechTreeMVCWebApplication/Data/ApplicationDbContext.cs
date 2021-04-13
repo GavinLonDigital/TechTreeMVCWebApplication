79aa8a4f4f96932724282266eff81e4fe3e21b92
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2021-04-13 14:43:25 +0200

    Initial commit

---
+++ TechTreeMVCWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechTreeMVCWebApplication.Data
+{
+    public class ApplicationUser:IdentityUser
+    { 
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string PostCode { get; set; }
+
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
