1da6ee5d93cf6ce641d1a658460a387bff49eaf5
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2021-04-20 12:18:03 +0200

    Part 5 - Added StringLength attribute to properties in ApplicationUser class

--- TechTreeMVCWebApplication/Data/ApplicationDbContext.cs
+++ TechTreeMVCWebApplication/Data/ApplicationDbContext.cs
@@ -3,20 +3,23 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+
 
 namespace TechTreeMVCWebApplication.Data
 {
     public class ApplicationUser:IdentityUser
     { 
+        [StringLength(250)]
         public string FirstName { get; set; }
-
+        [StringLength(250)]
         public string LastName { get; set; }
-
+        [StringLength(250)]
         public string Address1 { get; set; }
-
+        [StringLength(250)]
         public string Address2 { get; set; }
-
+        [StringLength(50)]
         public string PostCode { get; set; }
 
     }
