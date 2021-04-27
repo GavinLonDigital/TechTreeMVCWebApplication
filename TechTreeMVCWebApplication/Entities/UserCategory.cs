8e7191803fdcfaa96f89775da74784e612ca8b70
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2021-04-27 13:10:38 +0200

    Part 6 - Creating the Custom Tables (Entity Models)

---
+++ TechTreeMVCWebApplication/Entities/UserCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechTreeMVCWebApplication.Entities
+{
+    public class UserCategory
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        
+        public int CategoryId { get; set; }
+   
+    }
+}
