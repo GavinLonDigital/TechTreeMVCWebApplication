d32d9aa750f17482900b99d071b7681574f25199
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2021-04-27 13:39:03 +0200

    Revert "Revert "Part 6 - Creating the Custom Tables (Entity Models)""
    This reverts commit 5ed1e45f8cb7c6f9080b4c57b2f1d419922e1f2e.

---
+++ TechTreeMVCWebApplication/Entities/CategoryItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechTreeMVCWebApplication.Entities
+{
+    public class CategoryItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200,MinimumLength =2)]
+        public string Title { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int MediaTypeId { get; set; }
+
+        public DateTime DateTimeItemReleased { get; set; }
+
+
+
+
+    }
+}
