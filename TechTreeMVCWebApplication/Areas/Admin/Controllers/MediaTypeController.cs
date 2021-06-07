3d624b951f730b10724fc4d58ce6b76c5b6b9e2f
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2021-06-07 13:52:18 +0200

    Part 12 - Fixed 'back to list' link for content entity. Enlarged description field for Category and CategoryItem entities. Enlarged HTMLContent field for Content entity. Scaffolded CRUD code for MediaType entity.

---
+++ TechTreeMVCWebApplication/Areas/Admin/Controllers/MediaTypeController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TechTreeMVCWebApplication.Data;
+using TechTreeMVCWebApplication.Entities;
+
+namespace TechTreeMVCWebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MediaTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MediaTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/MediaType
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MediaType.ToListAsync());
+        }
+
+        // GET: Admin/MediaType/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mediaType = await _context.MediaType
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mediaType == null)
+            {
+                return NotFound();
+            }
+
+            return View(mediaType);
+        }
+
+        // GET: Admin/MediaType/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/MediaType/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ThumbnailImagePath")] MediaType mediaType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mediaType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mediaType);
+        }
+
+        // GET: Admin/MediaType/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mediaType = await _context.MediaType.FindAsync(id);
+            if (mediaType == null)
+            {
+                return NotFound();
+            }
+            return View(mediaType);
+        }
+
+        // POST: Admin/MediaType/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ThumbnailImagePath")] MediaType mediaType)
+        {
+            if (id != mediaType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mediaType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MediaTypeExists(mediaType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mediaType);
+        }
+
+        // GET: Admin/MediaType/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mediaType = await _context.MediaType
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mediaType == null)
+            {
+                return NotFound();
+            }
+
+            return View(mediaType);
+        }
+
+        // POST: Admin/MediaType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mediaType = await _context.MediaType.FindAsync(id);
+            _context.MediaType.Remove(mediaType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MediaTypeExists(int id)
+        {
+            return _context.MediaType.Any(e => e.Id == id);
+        }
+    }
+}
