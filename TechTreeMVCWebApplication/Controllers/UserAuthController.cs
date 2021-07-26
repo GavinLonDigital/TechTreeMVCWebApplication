6811f84ae49348ae45e0e289613f4ac30735dbdb
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2021-07-26 13:09:25 +0200

    Part 17 - Added the Custom Registration Functionality

--- TechTreeMVCWebApplication/Controllers/UserAuthController.cs
+++ TechTreeMVCWebApplication/Controllers/UserAuthController.cs
@@ -69,7 +69,46 @@ namespace TechTreeMVCWebApplication.Controllers
             }
 
         }
+        
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterUser(RegistrationModel registrationModel)
+        {
+            registrationModel.RegistrationInValid = "true";
 
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = registrationModel.Email,
+                    Email = registrationModel.Email,
+                    PhoneNumber = registrationModel.PhoneNumber,
+                    FirstName = registrationModel.FirstName,
+                    LastName = registrationModel.LastName,
+                    Address1 = registrationModel.Address1,
+                    Address2 = registrationModel.Address2,
+                    PostCode = registrationModel.PostCode
+
+                };
+
+                var result = await _userManager.CreateAsync(user, registrationModel.Password);
+
+                if (result.Succeeded)
+                {
+                    registrationModel.RegistrationInValid = "";
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return PartialView("_UserRegistrationPartial", registrationModel);
+                }
+
+                ModelState.AddModelError("", "Registration attempt failed");
+
+            }
+            return PartialView("_UserRegistrationPartial", registrationModel);
+
+        }
 
     }
 }
---
+++ TechTreeMVCWebApplication/wwwroot/js/UserRegistration.js
@@ -0,0 +1,66 @@
+﻿$(function () {
+    var registerUserButton = $("#UserRegistrationModal button[name = 'register']").click(onUserRegisterClick);
+
+    function onUserRegisterClick()
+    {
+        var url = "/UserAuth/RegisterUser";
+
+        var antiForgeryToken = $("#UserRegistrationModal input[name='__RequestVerificationToken']").val();
+        var email = $("#UserRegistrationModal input[name='Email']").val();
+        var password = $("#UserRegistrationModal input[name='Password']").val();
+        var confirmPassword = $("#UserRegistrationModal input[name='ConfirmPassword']").val();
+        var firstName = $("#UserRegistrationModal input[name='FirstName']").val();
+        var lastName = $("#UserRegistrationModal input[name='LastName']").val();
+        var address1 = $("#UserRegistrationModal input[name='Address1']").val();
+        var address2 = $("#UserRegistrationModal input[name='Address2']").val();
+        var postCode = $("#UserRegistrationModal input[name='PostCode']").val();
+        var phoneNumber = $("#UserRegistrationModal input[name='PhoneNumber']").val();
+
+        var user = {
+            __RequestAntiForgeryToken: antiForgeryToken,
+            Email: email,
+            Password: password,
+            ConfirmPassword: confirmPassword,
+            FirstName: firstName,
+            LastName: lastName,
+            Address1: address1,
+            Address2: address2,
+            PostCode: postCode,
+            PhoneNumber: phoneNumber,
+            AcceptUserAgreement: true
+        };
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: user,
+            success: function (data) {
+
+                var parsed = $.parseHTML(data);
+
+                var hasErrors = $(parsed).find("input[name='RegistrationInValid']").val() == 'true';
+
+                if (hasErrors) {
+
+                    $("#UserRegistrationModal").html(data);
+                    var registerUserButton = $("#UserRegistrationModal button[name = 'register']").click(onUserRegisterClick);
+
+                    $("#UserRegistrationForm").removeData("validator");
+                    $("#UserRegistrationForm").removeData("unobtrusiveValidation");
+                    $.validator.unobtrusive.parse("#UserRegistrationForm");
+                }
+                else {
+                    location.href = '/Home/Index';
+                }
+
+            },
+            error: function (xhr,ajaxOptions,thrownError) {
+
+                console.error(thrownError + '\r\n' + xhr.statusText + '\r\n' + xhr.responseText);
+            }
+
+        });
+
+    }
+
+});
