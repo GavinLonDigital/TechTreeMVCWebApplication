303e04b6a17b31a7eed44c60ee5f28378758b4b3
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2021-08-11 05:20:16 +0200

    Part 19 - UserCategory functionality

---
+++ TechTreeMVCWebApplication/Comparers/CompareUsers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using TechTreeMVCWebApplication.Areas.Admin.Models;
+
+namespace TechTreeMVCWebApplication.Comparers
+{
+    public class CompareUsers : IEqualityComparer<UserModel>
+    {
+        public bool Equals(UserModel x, UserModel y)
+        {
+            if (y == null) return false;
+
+            if (x.Id == y.Id)
+                return true;
+
+            return false;
+
+        }
+
+        public int GetHashCode([DisallowNull] UserModel obj)
+        {
+           return obj.Id.GetHashCode();
+        }
+    }
+}
--- TechTreeMVCWebApplication/Entities/Category.cs
+++ TechTreeMVCWebApplication/Entities/Category.cs
@@ -4,10 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using TechTreeMVCWebApplication.Interfaces;
 
 namespace TechTreeMVCWebApplication.Entities
 {
-    public class Category
+    public class Category:IPrimaryProperties
     {
         public int Id { get; set; }
         
