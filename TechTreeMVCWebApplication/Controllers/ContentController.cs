89656147d4e98a4902ec27ef2f4565cbb17a440c
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2021-08-23 16:48:32 +0200

    Part 22 - Added the code so that a logged on user can view content associated with a category item.

---
+++ TechTreeMVCWebApplication/Controllers/ContentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechTreeMVCWebApplication.Data;
+using TechTreeMVCWebApplication.Entities;
+
+namespace TechTreeMVCWebApplication.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int categoryItemId)
+        {
+            Content content = await (from item in _context.Content
+                                     where item.CategoryItem.Id == categoryItemId
+                                     select new Content
+                                     {
+                                         Title = item.Title,
+                                         VideoLink = item.VideoLink,
+                                         HTMLContent = item.HTMLContent
+                                     }).FirstOrDefaultAsync();
+
+            return View(content);
+        }
+    }
+}
